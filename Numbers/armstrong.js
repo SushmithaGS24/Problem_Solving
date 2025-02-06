c7b5b3427665cba5b921543df01486ad20192782
Sushmitha   G S <80742062+SushmithaGS24@users.noreply.github.com>
2025-02-06 09:27:12 +0530

    Add files via upload

--- Numbers/armstrong.js
+++ Numbers/armstrong.js
@@ -1,1 +1,27 @@
+function isArmstrong(n) {
+    let org = n;
+    let rem = 0;
+    let result = 0;
+    const count = Math.floor(Math.log10(n))+1;
 
+    
+    // Extract digits and store them in reverse order
+    while (n > 0) {
+        rem = n % 10;
+        result+= rem ** count;
+       
+        n = Math.floor(n / 10);
+    }
+
+    if(org ==+ result){
+        return true;
+    }
+    else {
+        return false;
+    }
+
+
+   
+}
+
+console.log(isArmstrong(153));
